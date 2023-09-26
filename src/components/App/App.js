36a2344669e03a26bf9257f0b3a0f390cde8c533
Logan Cole <emtlogancole@gmail.com>
2023-09-26 15:31:56 -0500

    reorganize files

---
+++ src/components/App/App.js
@@ -0,0 +1,20 @@
+import { useState } from 'react'; 
+import './App.css';
+
+function App()  {
+  const dummyIdeas = [
+    { id: 1, title: 'Prank Travis', description: 'Stick googly eyes on all his stuff' },
+    { id: 2, title: 'Make a secret password app', description: 'So you and your rideshare driver can both know neither one of you is lying' },
+    { id: 3, title: 'Learn a martial art', description: 'To exact vengeance upon my enemies' },
+  ]
+  const [ideas, setIdeas] = useState(dummyIdeas);
+
+  return (
+    <main>
+    <h1>IdeaBox</h1>
+    <p>Hello World</p>
+    </main>
+  )
+}
+
+export default App;
